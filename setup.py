368048096fdca5c2760b11821509d3f31e877d28
Herman Polloni <hpolloni@gmail.com>
2025-02-01 13:36:13 -0800

    Update Python version support
    - Drop Python 3.7 support (EOL)
    - Add support for Python 3.11 and 3.12
    - Specify Python version requirements in setup.py
    - Update GitHub Actions workflow

--- setup.py
+++ setup.py
@@ -14,5 +14,13 @@ setup(
     license="MIT",
     url='https://github.com/hpolloni/rdsline',
     long_description='RDS REPL',
-    install_requires=['pyyaml', 'boto3', 'tabulate']
+    install_requires=['pyyaml', 'boto3', 'tabulate'],
+    python_requires='>=3.8',
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
 )
